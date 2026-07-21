3708cf98b183eb6f3df59855ad2d8a4d0e482187
skochy-cyber <skochy-cyber@users.noreply.github.com>
2026-07-21 22:38:40 +0000

    v19.3 — Added MongoDB URI, app name Tribal AI

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ require('dotenv').config();
 const app        = express();
 const PORT       = process.env.PORT || 3000;
 const JWT_SECRET = process.env.JWT_SECRET || 'd2f2a84faf1d4b982f6f4fd25ed2fefbee860aa8689ab2e1826e762d7c60068a';
-const MONGO_URI  = process.env.MONGO_URI;
+const MONGO_URI  = process.env.MONGO_URI || 'mongodb+srv://mcht-cbt-system:***@cluster0.2mkue2i.mongodb.net/mcht-icbt?retryWrites=true&w=majority&appName=TribalTestProTnC';
 const ADMIN_EMAIL    = process.env.ADMIN_EMAIL || 'obasanjosamuel404@gmail.com';
 const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD || 'Obasanjo444@@';
 const ALLOWED_ORIGIN = process.env.ALLOWED_ORIGIN || '*';
